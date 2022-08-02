6e139ff4fab72e513cab979250837b7a7e63fe18
LeeJoEun-01 <joeun1005@naver.com>
2022-08-02 09:09:18 +0900

    Complete GET API

--- spring_introduction/hello/src/main/java/com/example/hello/controller/GetApiController.java
+++ spring_introduction/hello/src/main/java/com/example/hello/controller/GetApiController.java
@@ -1,6 +1,9 @@
 package com.example.hello.controller;
 
+import com.example.hello.dto.UserRequest;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/get")
@@ -31,5 +34,47 @@ public class GetApiController {
         return pathName;
     }
 
-    //쿼리 파라미터 받는 방법
+    // 쿼리 파라미터 받는 방법
+    // http://localhost:9090/api/get/query-param?user=joeun&email=joeun1005@naver.com&aga=22
+
+    //방법1) Map으로 받기
+    @GetMapping(path= "query-param")
+    public String queryParam(@RequestParam Map<String, String> queryParam){
+        StringBuilder sb = new StringBuilder();
+
+        queryParam.entrySet().forEach( entry ->{
+            System.out.println(entry.getKey());
+            System.out.println(entry.getValue());
+            System.out.println("\n");
+
+            sb.append(entry.getKey()+" = "+entry.getValue()+"\n");
+        });
+
+        return sb.toString();
+    }
+
+    //방법2) 변수마다 @RequestParam으로 받는 법 (너무 번거로움)
+    @GetMapping(path = "query-param02")
+    public String queryParam02(
+            @RequestParam String name,
+            @RequestParam String email,
+            @RequestParam String age
+    ){
+        System.out.println(name);
+        System.out.println(email);
+        System.out.println(age);
+
+        return name+" "+email+" "+age;
+    }
+
+    //방법3) DTO 형태로 방법 (현업에서 제일 많이 씀)
+    @GetMapping(path = "query-param03")
+    public String queryParam03(UserRequest userRequest){
+        System.out.println(userRequest.getName());
+        System.out.println(userRequest.getEmail());
+        System.out.println(userRequest.getAge());
+
+        return userRequest.toString();
+    }
+
 }
